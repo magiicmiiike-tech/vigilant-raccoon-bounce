8714373ca4ef41c80727a18df149a75b35844be8
[dyad] <icpywrite@gmail.com>
2026-01-07 07:58:34 -0500

    [dyad] Generate database migration file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,1 +1,9 @@
-export const migrations = []
+import * as migration_20260107_125833 from './20260107_125833';
+
+export const migrations = [
+  {
+    up: migration_20260107_125833.up,
+    down: migration_20260107_125833.down,
+    name: '20260107_125833'
+  },
+];
